33b9ef6d3c55218cb3df2f5f26f0255d81d1d461
Annie Hedgpeth <annie@hedge-ops.com>
2023-12-27 13:07:50 -0700

    added queries

--- src/lib/queries.sql
+++ src/lib/queries.sql
@@ -21,16 +21,51 @@ INSERT INTO quizzes(title, description, question_text) values(
          'What is a variable?'
         );
 
+INSERT INTO quizzes(title, description, question_text) values(
+                                                                 'Programming Quiz 2',
+                                                                 'A quiz about programming concepts',
+                                                                 'What is a variable?'
+                                                             );
+
+INSERT INTO quizzes(title, description, question_text) values(
+                                                                 'Programming Quiz 3',
+                                                                 'A quiz about programming concepts',
+                                                                 'What is a variable?'
+                                                             );
+
 SELECT * FROM quizzes;
+DELETE FROM quizzes WHERE 1=1;
+
+-- Quiz 1
+INSERT INTO answers(quiz_id, answer_text, is_correct)
+             VALUES(5, 'A container for storing data', true);
 
 INSERT INTO answers(quiz_id, answer_text, is_correct)
-             VALUES(1, 'A container for storing data', true);
+VALUES(5, 'A programming language', false);
 
 INSERT INTO answers(quiz_id, answer_text, is_correct)
-VALUES(1, 'A programming language', false);
+VALUES(5, 'A mathematical operation', false);
+
+-- Quiz 2
+INSERT INTO answers(quiz_id, answer_text, is_correct)
+VALUES(6, 'A container for storing data', true);
 
 INSERT INTO answers(quiz_id, answer_text, is_correct)
-VALUES(1, 'A mathematical operation', false);
+VALUES(6, 'A programming language', false);
+
+INSERT INTO answers(quiz_id, answer_text, is_correct)
+VALUES(6, 'A mathematical operation', false);
+
+-- Quiz 3
+INSERT INTO answers(quiz_id, answer_text, is_correct)
+VALUES(7, 'A container for storing data', true);
+
+INSERT INTO answers(quiz_id, answer_text, is_correct)
+VALUES(7, 'A programming language', false);
+
+INSERT INTO answers(quiz_id, answer_text, is_correct)
+VALUES(7, 'A mathematical operation', false);
+
 
 SELECT
     q.quiz_id,
@@ -43,3 +78,5 @@ SELECT
 FROM quizzes AS q
          JOIN answers AS a ON q.quiz_id = a.quiz_id
 WHERE q.quiz_id = 1;
+
+DELETE FROM answers WHERE 1=1;
